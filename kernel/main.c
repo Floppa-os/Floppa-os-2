18472b21c0140ffc30bc8734744ce54bc0b5b9a4
Mickredset <aivliev782@gmail.com>
2026-01-04 17:31:17 +0300

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,15 +1,29 @@
 #include "include/kernel.h"
+#include "include/task.h"
 
 void kernel_main() {
     vga_write("Floppa OS booting...\n", 0x07);
     init_paging();
     init_heap();
+    task_init();  // Инициализируем подсистему задач
+
+    // Создаём первую задачу
+    task_create(task_example);
+
     enable_interrupts();
 
     while (1) {
-        // Основной цикл ядра
+        schedule();  // Основной цикл планировщика
     }
 }
 
-void kernel_main() {
-    vga_write
+// Пример задачи
+void task_example() {
+    int counter = 0;
+    while (1) {
+        vga_write("Task: ", 0x07);
+        vga_write(itoa(counter++), 0x07);
+        vga_write("\n", 0x07);
+        sys_task_yield();  // Добровольно отдаём управление
+    }
+}
