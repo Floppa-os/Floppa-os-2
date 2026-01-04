c988b0e24db9da9f4db5a68b88cb5b2c79d1aa95
Mickredset <aivliev782@gmail.com>
2026-01-04 17:47:45 +0300

    Create commands.c

---
+++ kernel/shell/commands.c
@@ -0,0 +1,67 @@
+#include "include/shell.h"
+#include "include/fs.h"
+
+// Список команд
+static struct {
+    const char *name;
+    void (*func)(int argc, char **argv);
+} commands[] = {
+    {"help", cmd_help},
+    {"reboot", cmd_reboot},
+    {"ls", cmd_ls},
+    {NULL, NULL}  // Конец списка
+};
+
+// Разбиваем строку на аргументы
+static int parse_args(char *cmd, char **argv) {
+    int argc = 0;
+    char *p = cmd;
+    while (*p) {
+        if (*p != ' ' && *p != '\t') {
+            argv[argc++] = p;
+            while (*p && *p != ' ' && *p != '\t') p++;
+            *p = '\0';
+        }
+        p++;
+    }
+    return argc;
+}
+
+void execute_command(char *cmd) {
+    char *argv[10];
+    int argc = parse_args(cmd, argv);
+
+    for (int i = 0; commands[i].name; i++) {
+        if (strcmp(argv[0], commands[i].name) == 0) {
+            commands[i].func(argc, argv);
+            return;
+        }
+    }
+    shell_puts("Unknown command: ");
+    shell_puts(argv[0]);
+    shell_puts("\n");
+}
+
+// Реализация команд
+void cmd_help(int argc, char **argv) {
+    shell_puts("Available commands:\n");
+    for (int i = 0; commands[i].name; i++) {
+        shell_puts("  ");
+        shell_puts(commands[i].name);
+        shell_puts("\n");
+    }
+}
+
+void cmd_reboot(int argc, char **argv) {
+    shell_puts("Rebooting...\n");
+    asm volatile("mov $0xFE, %%ah\n"
+                 "int $0x19"
+                 : : : "ah");
+}
+
+void cmd_ls(int argc, char **argv) {
+    // Пример: выводим файлы из корневого каталога FAT12
+    // (реальная реализация требует обхода FAT)
+    shell_puts("file1.txt\n");
+    shell_puts("kernel.bin\n");
+}
